d69808c6edcdf81eb9e65054d5adc820b12f1d05
LaupWing <loc_strict@hotmail.com>
2024-01-05 08:52:00 +0100

    updated author

--- 1-books/database/migrations/2024_01_04_174045_create_authors_table.php
+++ 1-books/database/migrations/2024_01_04_174045_create_authors_table.php
@@ -6,22 +6,25 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('authors', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+   /**
+    * Run the migrations.
+    */
+   public function up(): void
+   {
+      Schema::create("authors", function (Blueprint $table) {
+         $table->id();
+         $table->string("firstname");
+         $table->string("lastname");
+         $table->date("birth_date");
+         $table->timestamps();
+      });
+   }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('authors');
-    }
+   /**
+    * Reverse the migrations.
+    */
+   public function down(): void
+   {
+      Schema::dropIfExists("authors");
+   }
 };
