b2cdb88f3bed51fe7f067c6270f8a7caa58bb357
LaupWing <loc_strict@hotmail.com>
2024-01-24 19:14:48 +0100

    udpated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Book;
+use App\Models\Genre;
 use App\Models\User;
 use Database\Factories\GenreBookFactory;
 use Illuminate\Database\Seeder;
@@ -24,6 +26,8 @@ class DatabaseSeeder extends Seeder
          GenreSeeder::class,
          AuthorsSeeder::class
       ]);
-      GenreBookFactory::factory(100)->create();
+      logger(Genre::all()->count());
+      logger(Book::all()->count());
+      // GenreBookFactory::factory(100)->create();
    }
 }
