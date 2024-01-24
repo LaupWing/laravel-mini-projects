5a4a40c7f839acb2b250572885c95d38a38a2d04
laupwing <loc_strict@hotmail.com>
2024-01-24 18:33:45 +0100

    getting autor

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get("/books", function () {
     return [
-        "books" => Book::all()
+        "books" => Book::with("author")->get()
     ];
 });
