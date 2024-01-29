5bbce9e81e1d15a330c416746e053de64fac076d
laupwing <loc_strict@hotmail.com>
2024-01-29 08:50:13 +0100

    removed unused code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\Book;
-use App\Models\Genre;
 use App\Models\User;
 use Database\Factories\GenreBookFactory;
 use Illuminate\Database\Seeder;
