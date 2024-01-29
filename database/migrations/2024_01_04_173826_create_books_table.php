bdee7cdc6e70a8fc1aaaa7fabcef9f6655b88b58
laupwing <loc_strict@hotmail.com>
2024-01-29 08:44:19 +0100

    removed unused code

--- database/migrations/2024_01_04_173826_create_books_table.php
+++ database/migrations/2024_01_04_173826_create_books_table.php
@@ -21,10 +21,6 @@ return new class extends Migration
          $table->foreign("author_id")
             ->references("id")
             ->on("authors");
-         // $table->foreignId("genre_id");
-         // $table->foreign("genre_id")
-         //    ->references("id")
-         //    ->on("genres");
       });
    }
 
